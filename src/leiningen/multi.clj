f790575a43b08a3930465a628644deb389ea1aab
Matthew Maravillas <maravillas@gmail.com>
2010-08-02 16:11:07 -0400

    Extract multi-library-path function

--- src/leiningen/multi.clj
+++ src/leiningen/multi.clj
@@ -1,26 +1,29 @@
 (ns leiningen.multi
-  (:use [leiningen.deps :only [deps]]))
+  (:use [leiningen.deps :only [deps]]
+	[leiningen.test :only [test]]))
 
-;; Hook clean
-;; Put multi deps in /multi-lib
+(def task-whitelist ["deps" "test" "run" "compile" "jar" "uberjar"])
 
 ;; http://thinkrelevance.com/blog/2009/08/12/rifle-oriented-programming-with-clojure-2.html
 (defn- indexed
   [coll]
   (map vector (iterate inc 0) coll))
 
-(defn- run-deps
+(defn- multi-library-path
   [project]
   ;; Should the path be relative to the project root or the cwd?
   ;; The defaults in leiningen.core/defproject choose the latter, so I will as
   ;; well, but it seems incorrect.
   ;; TODO: Verify
-  (let [multi-library-path (or (:multi-library-path project)
-			       (str (:root project) "/multi-lib"))]
-    (deps project true)
-    (doseq [[index deps-set] (indexed (:multi-deps project))]
-      (deps (merge project {:library-path (str multi-library-path "/set" index)
-			    :dependencies deps-set}) true))))
+  (or (:multi-library-path project)
+      (str (:root project) "/multi-lib")))
+
+(defn- run-deps
+  [project]
+  (deps project true)
+  (doseq [[index deps-set] (indexed (:multi-deps project))]
+    (deps (merge project {:library-path (str (multi-library-path project) "/set" index)
+			  :dependencies deps-set}) true)))
 
 (defn multi
   [project task & args]
