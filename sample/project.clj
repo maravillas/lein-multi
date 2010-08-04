1a81b1d84e168c2da711e335fe3c85df900ef8ed
Matthew Maravillas <maravillas@gmail.com>
2010-08-04 12:01:27 -0400

    Add warning to sample project.clj

--- sample/project.clj
+++ sample/project.clj
@@ -1,2 +1,5 @@
+;; This project and the files contained in its folders are support for the 
+;; tests in /test. Do not modify these files without also modifying the tests.
+
 (defproject lein-multi-test "0.1.0"
   :dependencies [[org.clojure/clojure "1.2.0-RC1"]])
