486cecab8d4db28f0f3fcf2712ef63de95f3ec55
Matthew Maravillas <maravillas@gmail.com>
2010-08-04 11:59:01 -0400

    Add a little metadata to project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject lein-multi "0.1.0-SNAPSHOT"
   :description "A Leiningen plugin for running tasks against multiple dependency sets."
+  :url "http://github.com/maravillas/lein-multi"
+  :license {:name "MIT License"
+            :url "http://www.opensource.org/licenses/mit-license.php"}
   :dev-dependencies [[org.clojure/clojure "1.2.0-RC1"]
 		     [org.clojure/clojure-contrib "1.2.0-RC1"]
 		     [lein-clojars "0.5.0-SNAPSHOT"]
