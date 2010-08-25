624c26c02e4de567c2329b733b435dc2ead69bef
Matthew Maravillas <maravillas@gmail.com>
2010-08-24 23:55:49 -0400

    Update deps to 1.2 final

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://github.com/maravillas/lein-multi"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dev-dependencies [[org.clojure/clojure "1.2.0-RC1"]
-		     [org.clojure/clojure-contrib "1.2.0-RC1"]
+  :dev-dependencies [[org.clojure/clojure "1.2.0"]
+		     [org.clojure/clojure-contrib "1.2.0"]
 		     [lein-clojars "0.5.0-SNAPSHOT"]
 		     [swank-clojure "1.2.1"]])
