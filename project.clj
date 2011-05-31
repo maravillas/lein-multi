f15eda89ec16188f3a6af853ab2148793d278a67
Paul deGrandis <paul.degrandis@gmail.com>
2011-05-31 10:46:45 -0700

    Upped the version number, pushing this to clojars

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-multi "1.0.0"
+(defproject lein-multi "1.0.1"
   :description "A Leiningen plugin for running tasks against multiple dependency sets."
-  :url "http://github.com/maravillas/lein-multi"
+  :url "http://github.com/ohpauleez/lein-multi"
   :license {:name "MIT License"
             :url "http://www.opensource.org/licenses/mit-license.php"}
-  :dev-dependencies [[org.clojure/clojure "1.2.0"]
-		     [org.clojure/clojure-contrib "1.2.0"]
-		     [lein-clojars "0.5.0-SNAPSHOT"]
+  :dev-dependencies [[org.clojure/clojure "1.2.1"]
+		     [org.clojure/clojure-contrib "1.2.1"]
 		     [swank-clojure "1.2.1"]])
+
